c993e4dd276b08d7ca0ad4fb2ed50f2a7b8a97e6
Mehtab Singh <mehtabsinghxx@gmail.com>
2025-02-28 15:48:49 +0000

    add vehicle to package

--- traffic_modelling/src/models/__init__.py
+++ traffic_modelling/src/models/__init__.py
@@ -2,5 +2,12 @@ from .BusCycleLane import BusCycleLane
 from .PedestrianCrossing import PedestrianCrossing
 from .Lane import Lane
 from .Junction import Junction
+from .Vehicle import Vehicle
 
-__all__ = ["BusCycleLane", "PedestrianCrossing", "Lane", "Junction"] 
+__all__ = [
+    "BusCycleLane",
+    "PedestrianCrossing",
+    "Lane",
+    "Junction",
+    "Vehicle",
+]
