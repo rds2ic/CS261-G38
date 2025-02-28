0cafebb6478eda6aae6e18d20afec663d34ca4f6
Mehtab Singh <mehtabsinghxx@gmail.com>
2025-02-28 12:21:39 +0000

    feat: add movement parameter

--- traffic_modelling/src/models/Vehicle.py
+++ traffic_modelling/src/models/Vehicle.py
@@ -1,11 +1,15 @@
 class Vehicle:
-    def __init__(self, entry, exit=None):
+    def __init__(self, entry : int, movement : str,exit : int=None):
         self.entry = entry
         self.exit = exit
         self.time = 0
+        self.movement = movement # "straight", "right", or "left"
         
     def getEntry(self):
         return self.entry
+    
+    def setExit(self, exit : int):
+        self.exit = exit
     
     def getExit(self):
         return self.exit
