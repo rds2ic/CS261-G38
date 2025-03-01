6f655d73395ce41b7da5e3f413c4ee568a8d0436
Mehtab Singh <mehtabsinghxx@gmail.com>
2025-03-01 15:46:34 +0000

    refactor: simplified logic

--- traffic_modelling/src/simulation/Simulation.py
+++ traffic_modelling/src/simulation/Simulation.py
@@ -43,72 +43,67 @@ class Simulation:
     def runSimulation(self):
         # For each direction and turn, compute the deterministic arrival times
         # Formula for scheduling vehicles: arrival_time = (3600 / vehicles per hour)
-        next_arrivals = {}     # next_arrivals[direction][movement] = next arrival time
-        arrival_intervals = {} # arrival_intervals[direction][movement] = time between arrivals
-        movements = ["straight", "right", "left"]
+        next_arrivals = {}     # next_arrivals[direction] = next arrival time
+        arrival_intervals = {} # arrival_intervals[direction] = time between arrivals
 
         # Initialize next_arrivals and arrival_intervals
-        for direction, flows in self.traffic_data.items():
-            next_arrivals[direction] = {}
-            arrival_intervals[direction] = {}
-            for i, m in enumerate(movements):
-                flow = flows[i]
-                if flow > 0:
-                    interval = 3600 / flow # seconds between arrivals
-                    next_arrivals[direction][m] = 0.0
-                else:
-                    interval = None
-                    next_arrivals[direction][m] = None
-                arrival_intervals[direction][m] = interval
+        for direction, flow in self.traffic_data.items():
+            total_flow = sum(flow)
+            if total_flow > 0:
+                interval = 3600 / total_flow # seconds betweena arrivals for that direction
+                next_arrivals[direction] = 0
+                arrival_intervals[direction] = interval
+            else:
+                next_arrivals[direction] = None
+                arrival_intervals[direction] = None
         
         # Fixed turning cost
         right_turn_cost = 2
         left_turn_cost = 1
 
+        # Main simulation loop
         for t in range(self.simulation_duration):
             self.time = t
 
-            # Add new vehicle for arrival
             for direction in ["north", "south", "east", "west"]:
-                for m in movements:
-                    interval = arrival_intervals[direction][m]
-                    if interval is None:
-                        continue
+                interval = arrival_intervals[direction]
+                if interval is None:
+                    continue
 
-                    while next_arrivals[direction][m] is not None and next_arrivals[direction][m] <= t:
-                        # Add vehicle to queue
-                        arrival_time = next_arrivals[direction][m]
-                        vehicle = Vehicle(entry=arrival_time, movement=m)
-                        self.queues[direction].append(vehicle)
-                        # Schedule next arrival
-                        next_arrivals[direction][m] += interval
+                while next_arrivals[direction] is not None and next_arrivals[direction] <= t:
+                    # Create a new vehicle
+                    vehicle = Vehicle(t, direction)
+                    self.queues[direction].append(vehicle)
+
+                    # Schedule next arrival
+                    next_arrivals[direction] += interval
                 
                 # Sort queue by arrival time
                 self.queues[direction].sort(key=lambda v: v.entry)
-                # Update the maximum queue length
-                if len(self.queues[direction]) > self.max_queue_lengths[direction]:
-                    self.max_queue_lengths[direction] = len(self.queues[direction])
+
+                # Updae the maximum queue length
+                self.max_queue_lengths[direction] = max(self.max_queue_lengths[direction], len(self.queues[direction]))
             
-            # Update the active phase of the traffic light
             phase_time = t % self.cycle_length
             if phase_time < (self.cycle_length / 2):
                 active_phase = ["north", "south"]
             else:
                 active_phase = ["east", "west"]
             
-            # Process vehicles in the queue
+            # Process the active phase
             for direction in active_phase:
                 if self.queues[direction]:
                     vehicle = self.queues[direction].pop(0)
-
-                    # Determine cost depending on turn direction
+                    
                     if vehicle.movement == "right":
                         cost = right_turn_cost
                     elif vehicle.movement == "left":
                         cost = left_turn_cost
                     else:
                         cost = 0
-                    exit_time = t + cost
-                    vehicle.setExit(exit_time)
-                    wait_time = vehicle.getWaitTime()
-                    self.wait_times[direction].append(wait_time)
+                    
+                exit_time = t + cost
+                vehicle.setExit(exit_time)
+                wait_time = vehicle.getWaitTime()
+                self.wait_times[direction].append(wait_time)
+
